d5f6db8049f87b6487602a6b25c10aa3450238b8
Muhammed <70836304+muhorinho@users.noreply.github.com>
2023-10-07 12:10:35 +0300

    lab 1

---
+++ app/src/main/java/com/muhammedbuga/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.muhammedbuga.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Add your login logic here
+                    Toast.makeText(MainActivity.this, "Login button clicked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
